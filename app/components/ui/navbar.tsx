06307196cfa9f3e110bfd515238bdf4d543945bb
Mega Suartika <mega@itsavirus.com>
2025-03-10 18:03:38 +0800

    update: navbar app title

--- app/components/ui/navbar.tsx
+++ app/components/ui/navbar.tsx
@@ -7,7 +7,7 @@ export function Navbar() {
     <nav className='bg-background/95 text-foreground supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 h-14 outline-1 drop-shadow-md backdrop-blur outline-dashed'>
       <div className='main-container flex h-full items-center'>
         <NavLink to='/' className='mr-2 text-xl font-extrabold'>
-          New<span className='text-purple-700'>App</span>
+          News<span className='text-purple-700'>Aggregator</span>
         </NavLink>
         <div className='flex w-full flex-1 justify-end'>
           <NavigationMenu>
