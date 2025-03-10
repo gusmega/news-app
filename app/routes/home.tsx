94e7678d905ec47aa16d5ab543be72fb9d821140
Mega Suartika <mega@itsavirus.com>
2025-03-10 21:45:14 +0800

    feat: show article on the homepage

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,37 @@
+import { useQuery } from '@tanstack/react-query'
+
+import { fetchNewsApi } from '~/api/fetch-news-api'
+import NewsItem from '~/components/ui/news-item'
+
 import type { Route } from './+types/home'
 
 // eslint-disable-next-line no-empty-pattern
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: 'New React Router App' },
-    { name: 'description', content: 'Welcome to React Router!' },
+    { title: 'News Aggregator App' },
+    { name: 'description', content: 'Welcome to News Aggregator App!' },
   ]
 }
 
-export default function Home() {
-  return <p className='text-center'>Home</p>
+export async function loader() {
+  return await fetchNewsApi({})
+}
+
+export default function Home({ loaderData }: Route.ComponentProps) {
+  const { data } = useQuery({
+    queryKey: ['news'],
+    queryFn: () => fetchNewsApi({}),
+    initialData: loaderData,
+    staleTime: 1000,
+  })
+
+  if (!data) return null
+
+  return (
+    <div className='grid gap-4 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'>
+      {data.map((article) => (
+        <NewsItem key={article.url} article={article} />
+      ))}
+    </div>
+  )
 }
